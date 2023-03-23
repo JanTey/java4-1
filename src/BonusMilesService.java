4cc2a04b771d71c6cd57466c2aaa60c28379cdc4
Zhan Teyvans <teyvans@gmail.com>
2023-03-23 22:23:29 +0300

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-        public int calculate(int amount) {
-            int oneMile = 20;
-            int bonus = amount / oneMile;
+    public int calculate(int amount) {
+        int oneMile = 20;
+        int bonus = amount / oneMile;
         return bonus;
-        }
+    }
 }
