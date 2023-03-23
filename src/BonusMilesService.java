63d205a1e4b00d0a8741f42813b599b0b0a506ff
Zhan Teyvans <teyvans@gmail.com>
2023-03-23 03:07:40 +0300

    Inicial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+        public int calculate(int amount) {
+            int oneMile = 20;
+            int bonus = amount / oneMile;
+        return bonus;
+        }
+}
